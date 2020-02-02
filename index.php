e1cd513212cd3de22de31e29594017dc912ca0c8
Jonathan <jgnyeholt@gmail.com>
2020-02-02 18:47:59 -0500

    finished functionality

--- index.php
+++ index.php
@@ -3,13 +3,25 @@
   <head>
     <meta charset="utf-8">
     <title>Typing Game</title>
+    <link rel="stylesheet" href="normalize.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="site.css">
   </head>
   <body>
+    <header>
+      <h1>Typing Challenge</h1>
+      <nav>
+        <ul class="game-type">
+          <li><a id="words" class="game-option selected" href="#">Words</a></li>
+          <li><a id="phrases" class="game-option" href="#">Phrases</a></li>
+        </ul>
+      </nav>
+    </header>
     <main>
-      <div>
-        <h3>Completed: <span id="completed" data-complete="0">0</span></h3>
+      <div class="stats">
+        <h3>Words: <span id="completed" data-complete="0">0</span></h3>
+        <h3 class="wpm-display" title="Words per minute">WPM: <span id="WPM"></span></h3>
+        <h3 class="time-display">Time: <span id="min">00</span>:<span id="sec">00</span>.<span id="milli">00</span></h3>
       </div>
       <h2 id="word-container"></h2>
       <div>
@@ -20,6 +32,7 @@
       </div>
     </main>
     <script src="words.js"></script>
+    <script src="phrases.js"></script>
     <script src="site.js"></script>
   </body>
 </html>
--- site.js
+++ site.js
@@ -2,10 +2,18 @@ document.addEventListener("DOMContentLoaded", () =>{
   init();
 });
 
+let wordArray = words.slice(0, words.length);
+let gameType = null;
+
 function init(){
   let wordContainer = document.getElementById("word-container");
   let typingContainer = document.getElementById("typing-container");
   const playButton = document.getElementById("play-button");
+  const wordButton = document.getElementById("words");
+  const phraseButton = document.getElementById("phrases");
+  let completeDisplay = document.getElementById("completed");
+  let wpmDisplay = document.getElementById("WPM");
+
   playButton.addEventListener("click", ()=>{
     if(playButton.value === "start"){
       typingContainer.disabled = false;
@@ -13,26 +21,21 @@ function init(){
       findWord();
       playButton.textContent = "Stop";
       playButton.value = "stop";
+      startTimer();
     } else if (playButton.value === "stop"){
-      typingContainer.disabled = true;
-      typingContainer.blur();
-      playButton.textContent = "Start";
-      playButton.value = "start";
+      resetGame();
     }
-
-  });
+  }); //end play button listener
 
   typingContainer.addEventListener("input", ()=>{
     let wordToType = wordContainer.textContent;
     let wordToTypeLength = wordToType.length;
     let userInput = typingContainer.value;
     let userInputLength = userInput.length;
-
     let subtringToMatch = wordToType.substring(0, userInputLength);
-
     //Length of input != word
     if(wordToTypeLength != userInputLength){
-      if(subtringToMatch.toLowerCase() == userInput.toLowerCase()){
+      if(subtringToMatch == userInput){
         typingContainer.style.backgroundColor = "#55efc4";
       }
       else {
@@ -40,23 +43,56 @@ function init(){
       }
     }
     //Length is same
-    else if(wordToTypeLength == userInputLength){
-      if(subtringToMatch.toLowerCase() == userInput.toLowerCase()){
+    else if (wordToTypeLength == userInputLength){
+      if(subtringToMatch == userInput){ //input is correct
+        let num = wordToType.split(" ").length;
         findWord();
         typingContainer.value = "";
         typingContainer.style.backgroundColor = "rgba(0,0,0,.05)";
-        incrementCompleted();
+        incrementCompleted(num);
+        calculateWPM();
       }
       else {
         typingContainer.style.backgroundColor = "#fab1a0";
       }
     }
+  }); //end typing listener
+
+  wordButton.addEventListener("click", ()=>{
+    wordArray = words.slice(0, words.length);
+    wordButton.classList.add("selected");
+    phraseButton.classList.remove("selected");
+    gameType = "words";
+    resetGame();
   });
+
+  phraseButton.addEventListener("click", ()=>{
+    wordArray = phrases.slice(0, phrases.length);
+    phraseButton.classList.add("selected");
+    wordButton.classList.remove("selected");
+    gameType = "phrases";
+    resetGame();
+  });
+
+  function resetGame(){
+    typingContainer.disabled = true;
+    typingContainer.blur();
+    typingContainer.value = "";
+    typingContainer.style.backgroundColor = "rgba(0,0,0,.05)";
+    playButton.textContent = "Start";
+    playButton.value = "start";
+    completeDisplay.dataset.complete = 0;
+    completeDisplay.textContent = 0;
+    wordContainer.textContent = "";
+    wpmDisplay.textContent = "";
+    stopTimer();
+  }
 }
 
 
 
 function findWord(){
+  resetTimer();
   let wordContainer = document.getElementById("word-container");
   //Find random word based on random number
   let wordArrayLength = wordArray.length;
@@ -71,27 +107,111 @@ function findWord(){
   } else {
     let typingContainer = document.getElementById("typing-container");
     const playButton = document.getElementById("play-button");
-    wordContainer.textContent = "There are no words left!!";
     typingContainer.disabled = true;
     typingContainer.blur();
     playButton.textContent = "Start";
     playButton.value = "start";
-    wordArray = words.slice(0, words.length);
+    if(gameType == "words"){
+      wordContainer.textContent = "There are no words left!!";
+      wordArray = words.slice(0, words.length);
+    } else if (gameType == "phrases"){
+      wordContainer.textContent = "There are no phrases left!!";
+      wordArray = phrases.slice(0, phrases.length);
+    }
   }
 }
 
-function incrementCompleted(){
+function incrementCompleted(numToIncrement){
   let completeDisplay = document.getElementById("completed");
   completeDisplay.style.backgroundColor = "#00b894";
+  completeDisplay.style.color = "white";
   let complete = Number(completeDisplay.dataset.complete);
-  let newCompleted = complete + 1;
+  let newCompleted = complete + numToIncrement;
   completeDisplay.dataset.complete = newCompleted;
   completeDisplay.textContent = newCompleted;
   setTimeout(function(){
     completeDisplay.style.transition = ".25s";
-    completeDisplay.style.backgroundColor = "white";
+    completeDisplay.style.backgroundColor = "transparent";
+    completeDisplay.style.color = "black";
   }, 250);
 
 }
 
-let wordArray = words.slice(0, words.length);
+
+let milliseconds = 0;
+let seconds = 0;
+let minutes = 0;
+let timerID;
+let minutesDisplay = document.getElementById("min");
+let secondsDisplay = document.getElementById("sec");
+let milliDisplay = document.getElementById("milli");
+let totalMilliseconds = 0; //Used for WPM
+
+
+function startTimer(){
+  timerID = setInterval(function(){
+    totalMilliseconds += 10;
+    milliseconds += 1;
+    milliseconds < 10 ? milliDisplay.textContent = "0" + milliseconds : milliDisplay.textContent = milliseconds;
+    if(milliseconds == 99){
+      seconds += 1;
+      milliseconds = 0;
+      milliDisplay.textContent = "00";
+      seconds < 10 ? secondsDisplay.textContent = "0" + seconds : secondsDisplay.textContent = seconds;
+    }
+    if(seconds == 59){
+      minutes += 1;
+      seconds = 0;
+      secondsDisplay.textContent = "00";
+      minutes < 10 ? minutesDisplay.textContent = "0" + minutes : minutesDisplay.textContent = minutes;
+
+    }
+  }, 10);
+}
+
+function stopTimer(){
+  console.log(timerID);
+  clearTimeout(timerID);
+  milliseconds = 0;
+  seconds = 0;
+  minutes = 0;
+  minutesDisplay.textContent = "00";
+  secondsDisplay.textContent = "00";
+  milliDisplay.textContent = "00";
+}
+
+function resetTimer(){
+  milliseconds = 0;
+  seconds = 0;
+  minutes = 0;
+  minutesDisplay.textContent = "00";
+  secondsDisplay.textContent = "00";
+  milliDisplay.textContent = "00";
+}
+
+function calculateWPM(){
+  let wpmDisplay = document.getElementById("WPM");
+  let currentWPM = wpmDisplay.textContent;
+  let wordsCompleted = document.getElementById("completed").dataset.complete;
+  let newWPM = (60000 * Number(wordsCompleted)) / totalMilliseconds;
+  wpmDisplay.textContent = Math.floor(newWPM);
+
+  //UI
+  if(newWPM < currentWPM){
+    wpmDisplay.style.backgroundColor = "#d63031";
+    wpmDisplay.style.color = "white";
+    setTimeout(function(){
+      wpmDisplay.style.transition = ".25s";
+      wpmDisplay.style.backgroundColor = "transparent";
+      wpmDisplay.style.color = "black";
+    }, 250);
+  } else {
+    wpmDisplay.style.backgroundColor = "#00b894";
+    wpmDisplay.style.color = "white";
+    setTimeout(function(){
+      wpmDisplay.style.transition = ".25s";
+      wpmDisplay.style.backgroundColor = "transparent";
+      wpmDisplay.style.color = "black";
+    }, 250);
+  }
+}
